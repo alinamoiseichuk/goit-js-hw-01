a0d30f8fd38117d1fc34e85028b878cd4f407b18
alinamoiseichuk <alinamoyseychuk8@gmail.com>
2024-01-21 16:40:54 +0100

    +

--- js/task-2.js
+++ js/task-2.js
@@ -6,3 +6,166 @@ return (`Shipping to ${country} will cost ${totalPrice} credits`)
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
 console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+
+
+
+// Функція checkForSpam(message) приймає рядок (параметр message), перевіряє його на вміст 
+// заборонених слів spam і sale, і повертає результат перевірки. Слова в рядку параметра message можуть 
+// бути в довільному регістрі, наприклад SPAM або sAlE.
+
+// Доповни код функції таким чином, що:
+
+//     Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
+//     Якщо в рядку відсутні заборонені слова, функція повертає буль false
+
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+
+// console.log(checkForSpam("Latest technology news")); // false
+// console.log(checkForSpam("JavaScript weekly newsletter")); // false
+// console.log(checkForSpam("Get best sale offers now!")); // true
+// console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+// console.log(checkForSpam("Trust me, this is not a spam message")); // true
+// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+// Залиш цей код для перевірки ментором.
+// На що буде звертати увагу ментор при перевірці:
+
+//     Оголошена функція checkForSpam(message).
+//     Виклик функції checkForSpam("Latest technology news") повертає false
+//     Виклик функції checkForSpam("JavaScript weekly newsletter")повертає false
+//     Виклик функції checkForSpam("Get best sale offers now!") повертає true
+//     Виклик функції checkForSpam("Amazing SalE, only tonight!") повертає true
+//     Виклик функції checkForSpam("Trust me, this is not a spam message") повертає true
+//     Виклик функції checkForSpam("Get rid of sPaM emails. Our book in on sale!") повертає true
+//     Виклик функції checkForSpam("[SPAM] How to earn fast money?") повертає true
+
+
+
+//    function checkForSpam(message) {
+//   return message.toLowerCase().includes("spam") || message.toLowerCase().includes("sale")
+//    }
+
+// checkForSpam("Latest technology news")
+// console.log(checkForSpam("Latest technology news")); // false
+// console.log(checkForSpam("JavaScript weekly newsletter")); // false
+// console.log(checkForSpam("Get best sale offers now!")); // true
+// console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+// console.log(checkForSpam("Trust me, this is not a spam message")); // true
+// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+
+// Оголоси функцію formatMessage(message, maxLength), яка приймає рядок (параметр message) та перевіряє його довжину відповідно до заданої максимальної довжини (параметр maxLength).
+
+// Доповни код функції таким чином, що:
+
+//     Якщо довжина рядка дорівнює або менша за maxLength, то функція повертає початковий рядок без змін.
+//     Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength символів, додає трикрапку "..." в кінці та повертає обрізану версію.
+
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+
+// console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+// console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+// console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+// console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+// Залиш цей код для перевірки ментором.
+// На що буде звертати увагу ментор при перевірці:
+
+//     Оголошена функція formatMessage(message, maxLength)
+//     Виклик функції formatMessage("Curabitur ligula sapien", 16) повертає "Curabitur ligula..."
+//     Виклик функції formatMessage("Curabitur ligula sapien", 23) повертає "Curabitur ligula sapien"
+//     Виклик функції formatMessage("Vestibulum facilisis purus nec", 20) повертає "Vestibulum facilisis..."
+//     Виклик функції formatMessage("Vestibulum facilisis purus nec", 30) повертає "Vestibulum facilisis purus nec"
+//     Виклик функції formatMessage("Nunc sed turpis a felis in nunc fringilla", 15) повертає "Nunc sed turpis..."
+//     Виклик функції formatMessage("Nunc sed turpis a felis in nunc fringilla", 41) повертає "Nunc sed turpis a felis in nunc fringilla"
+
+
+// function formatMessage(message, maxLength) {
+//     let result;
+//     if ( message.length > maxLength){
+//       result = message.slice(0, maxLength) + '...'; 
+//     } else{
+//       result = message.slice(0, message.length)
+//     }
+//     return result;
+// }
+
+
+// formatMessage("Curabitur ligula sapien", 16)
+// console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+// console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+// console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+// console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+
+// Оголоси функцію getShippingCost(country), яка повинна перевіряти можливість доставки товару в країну користувача (параметр country) 
+// і повертати повідомлення про результат. Обов'язково використовуй інструкцію switch.
+
+// Формат рядка, що повертається "Shipping to <country> will cost <price> credits", де замість <country> 
+// і <price> необхідно підставити відповідні значення.
+
+// Список країн і вартість доставки:
+
+//     China — 100 кредитів
+//     Chile — 250 кредитів
+//     Australia — 170 кредитів
+//     Jamaica — 120 кредитів
+
+// Зі списку видно, що доставка можлива не скрізь. Якщо зазначена країна відсутня 
+// у списку, то функція повинна повернути рядок "Sorry, there is no delivery to your country".
+
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. 
+// У консоль будуть виведені результати її роботи.
+
+// console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+// console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+// console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+// console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+// console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+// console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+
+// Залиш цей код для перевірки ментором.
+// На що буде звертати увагу ментор при перевірці:
+
+//     Оголошена функція getShippingCost(country)
+//     У тілі функції використана інструкція switch
+//     Виклик getShippingCost("Australia") повертає "Shipping to Australia will cost 170 credits"
+//     Виклик getShippingCost("Germany") повертає "Sorry, there is no delivery to your country"
+//     Виклик getShippingCost("China") повертає "Shipping to China will cost 100 credits"
+//     Виклик getShippingCost("Chile") повертає "Shipping to Chile will cost 250 credits"
+//     Виклик getShippingCost("Jamaica") повертає "Shipping to Jamaica will cost 120 credits"
+//     Виклик getShippingCost("Sweden") повертає "Sorry, there is no delivery to your country"
+
+function getShippingCost(country) {
+    let price
+    switch (country) {
+        case "China": price = 100;
+            return `Shipping to ${country} will cost ${price} credits`;
+            break;
+        case "Chile": price = 250; 
+           return `Shipping to ${country} will cost ${price} credits`;     
+            break;
+        case "Australia": price = 170;
+            return `Shipping to ${country} will cost ${price} credits`;
+            break;
+        case "Jamaica": price = 120;
+            return `Shipping to ${country} will cost ${price} credits`;
+            break;        
+        default:
+            return "Sorry, there is no delivery to your country"
+    }
+}
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
--- js/task-4.js
+++ js/task-4.js
@@ -18,16 +18,306 @@
 // console.log(checkAge(20))
 // console.log(checkAge(17))
 
-function checkStorage(available, ordered) {
-    const available = 200
-    const ordered = 50
-    if (available <= 200, ordered <= 50) {
-    console.log("Order is processed, our manager will contact you") 
-    return "Order is processed, our manager will contact you" }
-    else { 
-        console.log("Not enough goods in stock!")
-        return "Not enough goods in stock!"
-    }
-   }
+// function checkPassword(password) {
+//     const correctPassword = "jqueryismyjam";
+//     // if (password === correctPassword) {
+//     //   return "Access granted"
+//     // } else {
+//     //    return "Access denied, wrong password!"
+//     // }
+//     return password === correctPassword ? "Access granted!" : "Access denied, wrong password!"
+//   }
 
-   console.log(checkStorage(100, 50))
+//   console.log(checkPassword("jqueryismyjam"))
+//   console.log(checkPassword("angul4r1sl1f3"))
+   
+
+
+// function getSubscriptionPrice(type) {
+//     switch (type) {
+//       case "starter":
+//       return 0;
+//         break;
+//         case "professional":
+//             return 20
+//             break;
+//           case "organization":
+//             return 50
+//             break;
+//           default:
+//             return "Invalid subscription type!"
+      
+//     }
+//   }
+
+//   console.log(getSubscriptionPrice("starter"))
+//   console.log(getSubscriptionPrice("professional"))
+//   console.log(getSubscriptionPrice("organization"))
+//   console.log(getSubscriptionPrice("premium"))
+
+// console.log(5 && "und");
+// console.log( 0 && 5)
+
+// Функція isNumberInRange(start, end, number) перевіряє, чи входить число у проміжок. 
+// Вона оголошує три параметри, значення яких будуть задаватися під час її виклику:
+
+//     number - число, входження якого перевіряється
+//     start - початок числового проміжку
+//     end - кінець числового проміжку
+
+// Використовуючи оператор &&, доповни функцію isNumberInRange таким чином, 
+// щоб вона повертала результат входження number у числовий проміжок від start до end включно. 
+// Тобто число повинно бути одночасно і більшим або дорівнювати start, і меншим або дорівнювати end. 
+// Результатом виклику функції має бути буль true або false.
+
+//     Оголошена функція isNumberInRange(start, end, number)
+//     Використано оператор &&
+//     Виклик isNumberInRange(10, 30, 17) повертає true
+//     Виклик isNumberInRange(10, 30, 5) повертає false
+//     Виклик isNumberInRange(20, 50, 24) повертає true
+//     Виклик isNumberInRange(20, 50, 76) повертає false
+
+
+// function isNumberInRange(start, end, number) {
+//     return number >= start && number <= end
+//   }
+
+//   console.log(isNumberInRange(10, 30, 17))
+//   console.log(isNumberInRange(10, 30, 5)) 
+//   console.log(isNumberInRange(20, 50, 24))
+//  console.log(isNumberInRange(20, 50, 76))
+
+// Функція checkAccess(subType) перевіряє, чи може користувач отримати доступ до контенту. Перевірка відбувається за типом передплати. 
+// Використовуючи оператор "АБО", доповни код функції так, щоб якщо значення параметра subType дорівнює рядкам "pro" або "vip", 
+// то функція повертала true і користувач отримував доступ. В іншому випадку повертала false.
+
+//     Оголошена функція checkAccess(subType)
+//     Використано оператор ||
+//     Виклик checkAccess("pro") повертає true
+//     Виклик checkAccess("starter") повертає false
+//     Виклик checkAccess("vip") повертає true
+//     Виклик checkAccess("free") повертає false
+
+// function checkAccess(subType) {
+//     return (subType === "pro" || subType === "vip")
+// }
+
+// console.log(checkAccess("pro"))
+// console.log(checkAccess("starter"))
+// console.log(checkAccess("vip"))
+// console.log(checkAccess("free"))
+
+
+// function toggleModalVisibility(isVisible) {}
+
+// Функція toggleModalVisibility(isVisible) перемикає стан відображення модального вікна. 
+// У параметрі isVisible вона очікує буль true або false. Доповни код функції так, щоб вона повертала протилежне значення цього параметра.
+
+//     Оголошена функція toggleModalVisibility(isVisible)
+//     Виклик toggleModalVisibility(true) повертає false
+//     Виклик toggleModalVisibility(false) повертає true
+//     Використано оператор !
+
+    // function toggleModalVisibility(isVisible) {
+    //     return !isVisible 
+    // }
+
+    // console.log(toggleModalVisibility(true))
+    // console.log(toggleModalVisibility(false))
+
+//     Функція getSubstring(string, length) приймає рядок і повертає новий підрядок (його часткову копію). Вона оголошує два параметри, 
+//     значення яких будуть задаватися під час її виклику:
+
+//     string - оригінальний рядок
+//     length - довжина нового підрядка
+
+// Доповни код функції так, щоб вона повертала новий підрядок, який починається від початку string і має довжину length.
+
+//     Оголошена функція getSubstring(string, length)
+//     Виклик функції getSubstring("Hello world", 3) повертає "Hel"
+//     Виклик функції getSubstring("Hello world", 5) повертає "Hello"
+//     Виклик функції getSubstring("Hello world", 8) повертає "Hello wo"
+//     Виклик функції getSubstring("Hello world", 11) повертає "Hello world"
+//     Виклик функції getSubstring("Hello world", 0) повертає ""
+
+// function getSubstring(string) {
+// return string.slice(0, length);
+// }
+
+// getSubstring("Hello world", 3)
+// console.log(getSubstring("Hello world" )) /"Hel"
+// getSubstring("Hello world", 5) 
+// console.log(getSubstring("Hello world", 5)) /"Hello"
+// console.log(getSubstring("Hello world", 8)) /"Hello wo"
+// console.log(getSubstring("Hello world", 11)) /"Hello world"
+// console.log(getSubstring("Hello world", 0)) /""
+
+
+// const message = "Welcome to Bahamas!";
+// console.log(message.toUpperCase())
+// console.log(message.slice (1,5))
+
+
+// Функція normalizeInput(input, to) оголошує два параметри:
+
+//     input - рядок, який потрібно привести до певного регістру
+//     to - рядок с двома можливими значеннями: "upper" або "lower", що вказують до якого регістру потрібно привести значення input
+
+// Доповни код функції таким чином, щоб:
+
+//     Якщо значення параметра to — це рядок "upper", то функція повертала копію рядка input, але у верхньому регістрі
+//     В іншому разі, функція повертала копію рядка inputу нижньому регістрі
+
+//     Оголошена функція normalizeInput(input, to)
+//     Виклик функції normalizeInput("This ISN'T SpaM", "lower") повертає "this isn't spam"
+//     Виклик функції normalizeInput("This ISN'T SpaM", "upper") повертає "THIS ISN'T SPAM"
+//     Виклик функції normalizeInput("Big SALE", "lower") повертає "big sale"
+//     Виклик функції normalizeInput("Big SALE", "upper") повертає "BIG SALE"
+//     Виклик функції normalizeInput("Stay Awhile and Listen", "lower") повертає "stay awhile and listen"
+//     Виклик функції normalizeInput("Stay Awhile and Listen", "upper") повертає "STAY AWHILE AND LISTEN"
+
+// function normalizeInput(input, to) {
+//     if (to === "upper") {
+//         return input.toUpperCase()
+//     } else {
+//         return input.toLowerCase()
+//     }
+// }
+
+// normalizeInput("This ISN'T SpaM", "lower")
+// console.log(normalizeInput("This ISN'T SpaM", "lower"))
+// normalizeInput("This ISN'T SpaM", "upper")
+// console.log(normalizeInput("This ISN'T SpaM", "upper"))
+// console.log(normalizeInput("Big SALE", "lower"))
+// console.log(normalizeInput("Big SALE", "upper"))
+// console.log(normalizeInput("Stay Awhile and Listen", "lower"))
+// console.log(normalizeInput("Stay Awhile and Listen", "upper"))
+
+// Функція checkForName(fullName, firstName) приймає два параметри та повертає буль 
+// true або false - результат перевірки входження підрядка firstName у рядок fullName.
+
+//     fullName - рядок, що містить повне ім'я
+//     firstName - рядок, що містить ім'я для перевірки його входження в повне ім'я
+
+// Доповни тіло функції таким чином, щоб вона повертала результат перевірки входження імені (параметр firstName), 
+// у повне ім'я (параметр fullName). Врахуй, що регістр символів в параметрах fullName і firstName наперед не відомий. 
+// Перед перевіркою входження не забудь прирівняти їх до однакового регістру.
+
+//     Оголошена функція checkForName(fullname, firstName)
+//     Виклик функції checkForName("Jason Neis", "Jason") повертає true
+//     Виклик функції checkForName("Jason Neis", "jAsOn") повертає true
+//     Виклик функції checkForName("Jason Neis", "Jacob") повертає false
+//     Виклик функції checkForName("Caty Stars", "Caty") повертає true
+//     Виклик функції checkForName("Caty Stars", "cAtY") повертає true
+//     Виклик функції checkForName("Caty Stars", "Andromeda") повертає false
+
+// function checkForName(fullName, firstName) {
+//     // const full = fullName.toLowerCase();
+//     // const first = firstName.toLowerCase();
+//     // return fullName.includes(firstName);
+//     return fullName.includes(firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase());
+// }
+
+// console.log(checkForName("Jason Neis", "Jason"))
+// console.log(checkForName("Jason Neis", "jAsOn"))
+// console.log(checkForName("Jason Neis", "Jacob"))
+// console.log(checkForName("Caty Stars", "Caty"))
+// console.log(checkForName("Caty Stars", "cAtY"))
+// console.log(checkForName("Caty Stars", "Andromeda"))
+// console.log()
+// console.log()
+
+
+// Функція checkFileExtension(fileName, ext) приймає два параметри:
+
+//     fileName - рядок, що зберігає ім'я файлу з розширенням, наприклад, styles.css, hello.js тощо.
+//     ext - рядок, що зберігає розширення, наприклад, .css, .js тощо
+
+// Доповни код функції так, щоб:
+
+//     Якщо ім'я файлу fileName закінчується розширенням, зазначеним у параметрі ext, то функція повертала рядок "File extension matches"
+//     В іншому разі, функція повертала рядок "File extension does not match"
+
+//     Оголошена функція checkFileExtension(fullname, name)
+//     Виклик функції checkFileExtension("styles.css", ".css") повертає "File extension matches"
+//     Виклик функції checkFileExtension("styles.css", ".js") повертає "File extension does not match"
+//     Виклик функції checkFileExtension("app.js", ".js") повертає "File extension matches"
+//     Виклик функції checkFileExtension("app.js", ".html") повертає "File extension does not match"
+//     Виклик функції checkFileExtension("index.html", ".html") повертає "File extension matches"
+//     Виклик функції checkFileExtension("index.html", ".css") повертає "File extension does not match"
+//     Виклик функції checkFileExtension("index.html", ".js") повертає "File extension does not match"
+
+
+    // function checkFileExtension(fileName, ext) {
+    //     if (fileName.endsWith(ext))
+    //     {
+    //         return "File extension matches"
+    //     } else {
+    //        return "File extension does not match"
+    //     }
+    // } 
+
+    // checkFileExtension("styles.css", ".css")
+    // console.log(checkFileExtension("styles.css", ".css"))
+    // console.log(checkFileExtension("styles.css", ".js"))
+    // console.log(checkFileExtension("app.js", ".js"))
+    // console.log(checkFileExtension("app.js", ".html"))
+    // console.log(checkFileExtension("index.html", ".html"))
+    // console.log(checkFileExtension("index.html", ".css"))
+    // console.log(checkFileExtension("index.html", ".js"))
+
+//     Функція getFileName(file) приймає один параметр
+
+//     file - рядок з іменем файлу. Ім'я файлу може бути з розширенням, наприклад: styles.css, app.js або без, наприклад: styles, app.
+
+// Використовуючи методи indexOf та slice, доповни код функції таким чином, щоб:
+
+//     Вона перевіряла наявність розширення в імені файлу (назва розширення відокремлюється від імені файла крапкою)
+//     Якщо ім'я файлу не містить розширення, то функція повертала новий підрядок, що містить ім'я файлу без змін
+//     В іншому разі функція повертала підрядок з іменем файлу, але без розширення
+
+//     Оголошена функція getFileName(file)
+//     Виклик функції getFileName("styles.css") повертає "styles"
+//     Виклик функції getFileName("app.js") повертає "app"
+//     Виклик функції getFileName("app") повертає "app"
+//     Виклик функції getFileName("index.js") повертає "index"
+//     Виклик функції getFileName("index.html") повертає "index"
+//     Виклик функції getFileName("index.css") повертає "index"
+//     Виклик функції getFileName("index") повертає "index"
+
+    // function getFileName(file) {
+    // const index = file.indexOf(".")
+    // if (index === -1) {
+    //     return file
+    // } else {
+    //     return file.slice(0, file)
+    // }
+    // }
+
+    // getFileName("styles.css")
+    // console.log(getFileName("styles.css"))
+
+//     Функція createFileName(name, ext) приймає два параметри:
+
+//     name - рядок, що зберігає ім'я файлу без розширення, яке вводить користувач. Воно може містити зайві пробіли на початку 
+//     або в кінці рядка, наприклад "order ", " finance " тощо
+//     ext - рядок, що зберігає розширення, наприклад "txt", "xml" тощо
+
+// Використовуючи синтаксис шаблонних рядків і метод trim(), доповни код функції таким чином, щоб вона повертала повне 
+// (об'єднане) ім'я файлу з доданим розширенням, зазначеним у параметрі ext у форматі ім'я.розширення. 
+// Також повне ім'я файлу не повинно містити зайвих пробілів на початку або наприкінці.
+
+//     Оголошена функція createFileName(name, ext)
+//     Виклик функції createFileName(" order ", "txt") повертає "order.txt"
+//     Виклик функції createFileName("report ", "csv") повертає "report.csv"
+//     Виклик функції createFileName(" presentation", "xml") повертає "presentation.xml"
+
+    // function createFileName(name, ext) {
+    //     return `${name.trim()}.${ext.trim()}`
+    // }
+
+    // console.log(createFileName(" order ", "txt"))
+    // console.log(createFileName("report ", "csv"))
+    // console.log(createFileName(" presentation", "xml"))
+
+    
